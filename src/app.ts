fa47f17323fbba8455ecf27cc6c5915c4c53ab21
javadkh2 <khalilian.javad@gmail.com>
2019-03-25 12:03:05 +0430

    using simple mode or functor version for testing

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import { compose } from "../lib/FP-helpers"
 import { fMap, fValue } from "../lib/functor";
-import { convertListToArray, ListFunctor, range } from "../lib/list";
+import { convertListToArray, ListFunctor, range, pairMap } from "../lib/list";
 
 // FizzBuzz function
 const fizzBuzz = (n: number) => (n % 3 == 0 ? "Fizz" : "") + (n % 5 == 0 ? "Buzz" : "") || n;
 
-// a mapper that map a functor to fizzBuzz version of it
-const fizzBuzzMapper = fMap(fizzBuzz);
+function simple() {
 
-// console.log(convertListToArray(fizzBuzzMapper(ListFunctor(range(1, 15))).value()))
-const result = compose(convertListToArray, fValue, fizzBuzzMapper, ListFunctor)(range(1, 15));
-console.log(result);
+    const fizzBuzzMapper = pairMap(fizzBuzz);
+
+    const result = compose(convertListToArray, fizzBuzzMapper)(range(1, 15));
+
+    console.log(result);
+}
+
+function usingFunctor() {
+    // a mapper that map a functor to fizzBuzz version of it
+    const fizzBuzzMapper = fMap(fizzBuzz);
+
+    const result = compose(convertListToArray, fValue, fizzBuzzMapper, ListFunctor)(range(1, 15));
+
+    console.log(result);
+}
+
+simple()
+
