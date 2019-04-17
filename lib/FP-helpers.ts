28f39473bf180194a3cf0a55d04a00bb26f5b703
javadkh2 <khalilian.javad@gmail.com>
2019-04-17 10:36:42 +0430

    trampoline helper for tail call optimaization

--- lib/FP-helpers.ts
+++ lib/FP-helpers.ts
@@ -1,4 +1,4 @@
-// its mathematical function f o g = f(g(x))- compose n function to each other and return a function
+// it's mathematical function f o g = f(g(x))- compose n function to each other and return a function
 export const compose = (...func: Function[]) => (args: any) => func.reduceRight((arg, fn) => fn(arg), args)
 
 // curry return a function for treat with a function like a chain of functions with partial input
@@ -9,3 +9,16 @@ export const curry = (func: Function) => {
     }
     return chain([]);
 }
+
+// trampoline for TCO (Tail Call Optimization) on javascript till the engines implement it
+// wrap the main function to it and return a function instead of run it inside the main function
+// trampoline calls the returned function. With this trick the main function execution path finish
+// so the stack become free
+export const trampoline = (fn: any) => (...args: any[]) => {
+    let result = fn(...args);
+    while (typeof result == "function") {
+        result = result();
+    }
+    return result()
+}
+
